27057e3381167a58981d3defa0a017f5a077773d
Maurice <apostolmisa9@gmail.com>
2026-02-22 10:01:50 +0200

    params docs

--- Sorters/QuickSort.c
+++ Sorters/QuickSort.c
@@ -1,4 +1,25 @@
 #include "Base.h"
+/*
+ * @param arr
+ *      Pointer to the first element of the array to be sorted.
+ *      The array is modified in place.
+ *
+ * @param len
+ *      Number of elements in the array.
+ *
+ * @param size
+ *      Size (in bytes) of each element in the array.
+ *
+ * @param compar
+ *      Pointer to a comparison function.
+ *      The function must return:
+ *          < 0  if first element is less than second
+ *            0  if elements are equal
+ *          > 0  if first element is greater than second
+ *
+ *      Example signature:
+ *          int compar(const void* a, const void* b);
+*/
 void quick_sort(void* arr, size_t len, size_t size, cmp_func compar) {
     //TODO
 }
