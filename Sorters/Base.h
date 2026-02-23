e3cba3ae242107d4be5055afd4e6c41fc50a4d95
Gr0gu <danielchira111@gmail.com>
2026-02-23 11:47:24 +0000

    Bitonic Sort implementation (without parallelism)

--- Sorters/Base.h
+++ Sorters/Base.h
@@ -2,18 +2,24 @@
 #define BASE_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-    typedef int  (*cmp_func)(const void *, const void *);
-    typedef void (*sort_func_t)(void *arr, size_t len, size_t size, cmp_func compar);
+typedef int (*cmp_func)(const void*, const void*);
+typedef void (*sort_func_t)(void* arr,
+                            size_t len,
+                            size_t size,
+                            cmp_func compar);
 
-    void bubble_sort       (void *arr, size_t len, size_t size, cmp_func compar);
-    void quick_sort        (void *arr, size_t len, size_t size, cmp_func compar);
-    void merge_sort        (void *arr, size_t len, size_t size, cmp_func compar);
-    void radix_sort        (void *arr, size_t len, size_t size, cmp_func compar);
-    void bitonic_sort      (void *arr, size_t len, size_t size, cmp_func compar);
+void bubble_sort(void* arr, size_t len, size_t size, cmp_func compar);
+void quick_sort(void* arr, size_t len, size_t size, cmp_func compar);
+void merge_sort(void* arr, size_t len, size_t size, cmp_func compar);
+void radix_sort(void* arr, size_t len, size_t size, cmp_func compar);
+void bitonic_sort(void* arr, size_t len, size_t size, cmp_func compar);
 
 #ifdef __cplusplus
 }
--- Sorters/BitonicSort.c
+++ Sorters/BitonicSort.c
@@ -1,27 +1,64 @@
 #include "Base.h"
 
-/*
- * @param arr
- *      Pointer to the first element of the array to be sorted.
- *      The array is modified in place.
- *
- * @param len
- *      Number of elements in the array.
- *
- * @param size
- *      Size (in bytes) of each element in the array.
- *
- * @param compar
- *      Pointer to a comparison function.
- *      The function must return:
- *          < 0  if first element is less than second
- *            0  if elements are equal
- *          > 0  if first element is greater than second
- *
- *      Example signature:
- *          int compar(const void* a, const void* b);
-*/
+#define PARALLEL_THRESHOLD 4096
+
+/**
+ * direction:
+ * 1 -> ascending order
+ * 0 -> desceding order
+ */
+void comp_swap(void* arr,
+               size_t size,
+               int i,
+               int j,
+               int direction,
+               cmp_func comparator) {
+  void* elem_i = (char*)arr + (i * size);
+  void* elem_j = (char*)arr + (j * size);
+  int comp_res = comparator(elem_i, elem_j);
+
+  if ((direction == 1 && comp_res > 0) || (direction == 0 && comp_res < 0)) {
+    char temp[size];
+    memcpy(temp, elem_i, size);
+    memcpy(elem_i, elem_j, size);
+    memcpy(elem_j, temp, size);
+
+    free(temp);
+  }
+}
+
+void bitonic_merge(void* arr,
+                   size_t size,
+                   int start,
+                   int count,
+                   int direction,
+                   cmp_func comparator) {
+  if (count > 1) {
+    int k = count / 2;
+    for (int i = start; i < start + k; i++) {
+      comp_swap(arr, size, i, i + k, direction, comparator);
+    }
+
+    bitonic_merge(arr, size, start, k, direction, comparator);
+    bitonic_merge(arr, size, start + k, k, direction, comparator);
+  }
+}
+
+void _bitonic_sort(void* arr,
+                   size_t size,
+                   int start,
+                   int count,
+                   int direction,
+                   cmp_func comparator) {
+  if (count > 1) {
+    int k = count / 2;
+
+    _bitonic_sort(arr, size, start, k, 1, comparator);
+    _bitonic_sort(arr, size, start + k, k, 0, comparator);
+    bitonic_merge(arr, size, start, count, direction, comparator);
+  }
+}
 
 void bitonic_sort(void* arr, size_t len, size_t size, cmp_func compar) {
-    //TODO
+  _bitonic_sort(arr, size, 0, len, 1, compar);
 }
