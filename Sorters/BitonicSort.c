41b7b85fc6dc2a43297267192224b2e66d54ce86
Gr0gu <danielchira111@gmail.com>
2026-02-23 12:08:13 +0000

    Bitonic Search implementation (parallelism using OpenMP)

--- Sorters/BitonicSort.c
+++ Sorters/BitonicSort.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include "Base.h"
 
 #define PARALLEL_THRESHOLD 4096
@@ -22,8 +23,6 @@ void comp_swap(void* arr,
     memcpy(temp, elem_i, size);
     memcpy(elem_i, elem_j, size);
     memcpy(elem_j, temp, size);
-
-    free(temp);
   }
 }
 
@@ -35,12 +34,22 @@ void bitonic_merge(void* arr,
                    cmp_func comparator) {
   if (count > 1) {
     int k = count / 2;
+
+#pragma omp parallel for if (k > PARALLEL_THRESHOLD)
     for (int i = start; i < start + k; i++) {
       comp_swap(arr, size, i, i + k, direction, comparator);
     }
 
-    bitonic_merge(arr, size, start, k, direction, comparator);
-    bitonic_merge(arr, size, start + k, k, direction, comparator);
+    if (count > PARALLEL_THRESHOLD) {
+#pragma omp task
+      bitonic_merge(arr, size, start, k, direction, comparator);
+#pragma omp task
+      bitonic_merge(arr, size, start + k, k, direction, comparator);
+#pragma omp taskwait
+    } else {
+      bitonic_merge(arr, size, start, k, direction, comparator);
+      bitonic_merge(arr, size, start + k, k, direction, comparator);
+    }
   }
 }
 
@@ -53,12 +62,27 @@ void _bitonic_sort(void* arr,
   if (count > 1) {
     int k = count / 2;
 
-    _bitonic_sort(arr, size, start, k, 1, comparator);
-    _bitonic_sort(arr, size, start + k, k, 0, comparator);
+    if (count > PARALLEL_THRESHOLD) {
+#pragma omp task
+      _bitonic_sort(arr, size, start, k, 1, comparator);
+#pragma omp task
+      _bitonic_sort(arr, size, start + k, k, 0, comparator);
+#pragma omp taskwait
+    } else {
+      _bitonic_sort(arr, size, start, k, 1, comparator);
+      _bitonic_sort(arr, size, start + k, k, 0, comparator);
+    }
+
     bitonic_merge(arr, size, start, count, direction, comparator);
   }
 }
 
 void bitonic_sort(void* arr, size_t len, size_t size, cmp_func compar) {
-  _bitonic_sort(arr, size, 0, len, 1, compar);
+#pragma omp parallel
+  {
+#pragma omp single
+    {
+      _bitonic_sort(arr, size, 0, len, 1, compar);
+    }
+  }
 }
